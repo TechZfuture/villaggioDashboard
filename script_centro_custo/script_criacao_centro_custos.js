92ba9bc29abaf5d140f34efe85c0e879e291d2d0
Luiz Gustavo <luiz.gustavo@ethan.adm.br>
2023-10-22 23:46:38 -0300

    Finalizado

--- script_centro_custo/script_criacao_centro_custos.js
+++ script_centro_custo/script_criacao_centro_custos.js
@@ -1,64 +1,59 @@
-const mysql = require("mysql2/promise");
+const sql = require('mssql');
+const fetch = require('node-fetch');
 
 const dbConfig = {
-  host: "DESKTOP-SPTK1M6",
-  user: "",
-  password: "",
-  database: "sistema",
+  server: 'DESKTOP-SPTK1M6',
+  user: 'lgusstavo',
+  password: '!F1n@l12E*',
+  database: 'sistema',
 };
 
 // Função para buscar os dados da API
 async function buscarDadosDaAPI() {
-  const apiToken = "0C1F3E7F408A4B6B95ADCA50E36BDE9B";
+  const apiToken = '0C1F3E7F408A4B6B95ADCA50E36BDE9B';
   const apiUrl = `https://api.nibo.com.br/empresas/v1/costcenters?apitoken=${apiToken}`;
 
   try {
     const response = await fetch(apiUrl);
     if (!response.ok) {
-      throw new Error(
-        `Erro na solicitação: ${response.status} ${response.statusText}`
-      );
+      throw new Error(`Erro na solicitação: ${response.status} ${response.statusText}`);
     }
 
     const data = await response.json();
     return data.items; // Retorna apenas o array de objetos "items"
   } catch (error) {
-    console.error("Erro ao buscar dados da API:", error);
+    console.error('Erro ao buscar dados da API:', error);
     return [];
   }
 }
 
-// Função para inserir os dados no banco de dados
+// Função para inserir os dados no banco de dados SQL Server
 async function inserirDadosNoBancoDeDados(data) {
-  const connection = await mysql.createConnection(dbConfig);
-
   try {
-    for (const item of data) {
-      const [existe] = await connection.execute(
-        "SELECT * FROM cost_center WHERE cost_center_id = ?",
-        [item.costCenterId]
-      );
+    await sql.connect(dbConfig);
 
-      if (existe.length > 0) {         
-          await connection.execute("UPDATE cost_center SET description = ? WHERE cost_center_id = ?", [
-            item.description,
-            item.costCenterId
-          ]);    
+    for (const item of data) {
+      const existe = await sql.query`SELECT * FROM cost_center WHERE cost_center_id = ${item.costCenterId}`;
+
+      if (existe.recordset.length > 0) {
+        await sql.query`
+          UPDATE cost_center
+          SET description = ${item.description}
+          WHERE cost_center_id = ${item.costCenterId}
+        `;
       } else {
-        const query =
-          "INSERT INTO cost_Center (cost_center_id, description) VALUES (?, ?)";
-        await connection.query(query, [
-            item.costCenterId,
-            item.description
-        ]);
+        await sql.query`
+          INSERT INTO cost_center (cost_center_id, description)
+          VALUES (${item.costCenterId}, ${item.description})
+        `;
       }
     }
 
-    console.log("Dados inseridos no banco de dados com sucesso.");
+    console.log('Dados inseridos no banco de dados com sucesso.');
   } catch (error) {
-    console.error("Erro ao inserir dados no banco de dados:", error);
+    console.error('Erro ao inserir dados no banco de dados:', error);
   } finally {
-    connection.end(); // Feche a conexão com o banco de dados
+    await sql.close(); // Feche a conexão com o banco de dados SQL Server
   }
 }
 
@@ -70,6 +65,6 @@ async function inserirDadosNoBancoDeDados(data) {
       await inserirDadosNoBancoDeDados(dadosDaAPI);
     }
   } catch (error) {
-    console.error("Erro no processo:", error);
+    console.error('Erro no processo:', error);
   }
 })();
